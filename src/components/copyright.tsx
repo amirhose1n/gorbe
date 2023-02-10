0839e6087a3f0e0aafe5a8d5be270b331ffe797e
farhoodiamirhosein <farhoodiamirhosein@gmail.com>
2023-02-11 00:00:33 +0330

    fix

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -1,7 +1,7 @@
 export default function Copyright() {
   return (
     <p className="-mt-12 pl-12 pt-2 text-xs opacity-50  text-white z-50">
-      Powered by{" "}
+      Writen by{" "}
       <a
         className="text-xs cursor-pointer text-white"
         href="mailto:farhoodiamirhosein@gmail.com"
